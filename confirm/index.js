002c720bdb999f31ed0486fe33761f205bcf5116
Asher Sandler <52999326+asher-sandler@users.noreply.github.com>
2021-07-16 15:49:46 +0300

    2021-07-16

---
+++ confirm/index.js
@@ -0,0 +1,16 @@
+const is1995 = confirm("JavaScript появился в 1995 году?");
+if (is1995) {
+  alert("Верно");
+}
+
+const isECMA = confirm("Спецификация JavaScript называется ECMAScript?");
+
+if (isECMA) {
+  alert("Верно");
+}
+
+const isMonth = confirm("JavaScript был создан за 1 месяц?");
+
+if (isMonth) {
+  alert("За 10 дней!");
+}
---
+++ first-project/js-info.js
@@ -0,0 +1,5 @@
+alert("Самая первая реализация JavaScript была создана Бренданом Эйхом (Brendan Eich) в компании Netscape");
+alert("JavaScript был создан за 10 дней автором Бренданом Айком (Brandan Eich)");
+alert("В каком году появился JavaScript? В 1995.")
+alert("JavaScript это не Java");
+alert("Как называется спецификация JavaScript? ES6");
